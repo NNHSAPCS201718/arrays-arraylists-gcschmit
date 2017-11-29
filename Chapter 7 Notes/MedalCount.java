04947b33d3289024a6016fa5e201faadc8376833
Geoff Schmit <gcschmit@gmail.com>
2017-11-29 11:54:48 -0600

    finished 2D array example

--- Chapter 7 Notes/MedalCount.java
+++ Chapter 7 Notes/MedalCount.java
@@ -30,7 +30,7 @@ public class MedalCount
          * Alternative way to create a new 2D array. This would be
          *      followed by nested for loops to initialize each element.
          */
-        this.counts = new int[COUNTRIES][MEDALS];
+        //this.counts = new int[COUNTRIES][MEDALS];
     }
 
     /**
@@ -38,6 +38,59 @@ public class MedalCount
      */
     public void printTable()
     {
-        for( int row = 0; row < COUNTRIES; 
+        // good: for( int row = 0; row < COUNTRIES; row++ )
+        // better:
+        for( int row = 0; row < this.counts.length; row++ )
+        {
+            // good: for( int col = 0; col < MEDALS; col++ )
+            // better: for( int col = 0; col < this.counts[0].length; col++ )
+            // best:
+            for( int col = 0; col < this.counts[row].length; col++ )
+            {
+                System.out.print( this.counts[row][col] + "\t" );
+            }
+            
+            System.out.println();
+        }
     }
+    
+    /**
+     * Sum the medals for the specified country index
+     *
+     * @param  countryIndex  the index of the country in the table whose
+     *                          medals to sum
+     * @return the sum of the medals for the specified country index
+     */
+    public int sumMedalsForCountry(int countryIndex)
+    {
+        int sum = 0;
+        
+        for( int col = 0; col < this.counts[countryIndex].length; col++ )
+        {
+            sum += this.counts[countryIndex][col];
+        }
+        
+        return sum;
+    }
+    
+    /**
+     * Sum the medals of a given type specified by the medal index
+     *
+     * @param  medalIndex  the indesx of a given type of medal to sum
+     * @return the sum of the medals of a given type specified by the
+     *              medal index
+     */
+    public int sumMedalsForType(int medalIndex)
+    {
+        int sum = 0;
+        
+        for( int row = 0; row < this.counts.length; row++ )
+        {
+            sum += this.counts[row][medalIndex];
+        }
+        
+        return sum;
+    }
+
+
 }
